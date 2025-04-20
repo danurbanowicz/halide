5186f5f5ac31f89c2eb432bd0593b132ba2ecbd7
Dan Urbanowicz <dan.urbanowicz@gmail.com>
2025-04-20 20:27:08 +0200

    Update config.js

--- tina/config.js
+++ tina/config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from "tinacms";
 
-// Your hosting provider likely exposes this as an environment variable
+// Your hosting provider likely exposes this as an environment variable.
 const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
 
 export default defineConfig({
